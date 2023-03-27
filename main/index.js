4b13edac47a99c2abf2483e79eda79919cfde25e
oliverLo78 <oliverberto@gmail.com>
2023-03-27 10:41:55 -0400

    used materialize css link

--- main/index.js
+++ main/index.js
@@ -7,7 +7,13 @@ const generateHTML = ({ name, location, github, linkedin }) =>
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Document</title>
 </head>
 <body>
